3030ccf1f2ca4b6e933faf817180edff666681f8
devapefi <dev@ape.finance>
2022-05-14 02:14:10 +0800

    Update timelock period

--- deploy/002_deploy_timelock.ts
+++ deploy/002_deploy_timelock.ts
@@ -15,16 +15,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log: true,
   });
 
-  const twoDays = 60 * 60 * 24 * 2;
-  await deploy('48hrTimelock', {
+  const sevenDays = 60 * 60 * 24 * 7;
+  await deploy('7daysTimelock', {
     from: deployer,
     contract: 'TokenController',
-    args: [twoDays, [admin], [admin]],
+    args: [sevenDays, [admin], [admin]],
     log: true,
   });
 
   await execute('4hrTimelock', { from: deployer }, 'revokeRole', '0x5f58e3a2316349923ce3780f8d587db2d72378aed66a8261c916544fa6846ca5', deployer);
-  await execute('48hrTimelock', { from: deployer }, 'revokeRole', '0x5f58e3a2316349923ce3780f8d587db2d72378aed66a8261c916544fa6846ca5', deployer);
+  await execute('7daysTimelock', { from: deployer }, 'revokeRole', '0x5f58e3a2316349923ce3780f8d587db2d72378aed66a8261c916544fa6846ca5', deployer);
 
 };
 export default func;
